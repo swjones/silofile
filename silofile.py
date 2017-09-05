34c872e058ccc58ccf4465cb24d27d9d4a2aefb2
Samuel Jones <samuel.lucyk.jones@gmail.com>
2017-09-05 15:23:00 -0600

    docstring for module

--- silofile.py
+++ silofile.py
@@ -1,6 +1,26 @@
 import pyvisfile.silo as silo
 from warnings import warn
 
+"""
+Module for creating Silo-format files
+
+Examples
+--------
+>>> from silofile import silofile as sf
+create a 3d mesh:
+>>> x = np.linspace(0.,1.,11); y = np.linspace(0.,1.,11); z = np.linspace(0.,1.,11)
+>>> mesh = [ x, y, z ]
+create some 3d data:
+>>> xx,yy,zz = np.meshgrid(x,y,z)
+>>> spam = np.cos(xx) + 2.*np.sin(xx-yy) + 4.*np.cos(yy-zz)**2
+>>> jones = np.sqrt(np.abs(spam))
+create the object:
+>>> s = sf(mesh, ["spam","jones"], [spam,jones])
+write the file:
+>>> s.write("myfile.silo")
+"""
+
+
 class silofile():
     def __init__(self, mesh, keys, values):
         """object containing data to be written to a silo file
